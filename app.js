39094051f822f453d8c8b7bdb4f76fb8a4a81410
lettussandwich <csharman@student.unimelb.edu.au>
2024-03-08 21:08:02 +1100

    i'm getting lost in the sauce

--- app.js
+++ app.js
@@ -2,24 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//const path = require('path');
+const path = require('path');
+const fs = require('fs');
 
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, 'src', 'public')));
 
-const url = require('url');
-
-
-app.get('/', (req, res) => {
-    res.send("Hello, world!");
-})
-
-// // Serve static files from the 'public' folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Handle all other requests by sending the HTML file
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+// Handle all other requests by sending the HTML file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'public', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
