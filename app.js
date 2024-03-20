fb49bb2533e66802c25e33c5c7a3542a1de6069f
lettussandwich <csharman@student.unimelb.edu.au>
2024-03-20 17:53:11 +1100

    emailing added but not accepting credentials

--- app.js
+++ app.js
@@ -4,71 +4,11 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 const url = require('url');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 
 
 // Serve up the static, public files so that they can be used
 app.use(express.static(path.join(__dirname, '/src/public')));
-
-
-/** GET: returns an array of gallery image names */
-const galleryDir = path.join(__dirname, '/src/public/img/gallery');
-app.get('/api/gallery', (req, res) => {
-    fs.readdir(galleryDir, (err, files) => {
-        if(err){
-            console.log(`Error retrieving gallery folder: ${err}`);
-            return;
-        }
-
-        // remove README.txt if present
-        let index = files.indexOf("README.txt");
-        if(index >= 0) files.splice(index, 1);
-        
-        res.send(JSON.stringify(files));
-    })
-})
-
-
-/** GET: returns an array of heading image names */
-const headingImgDir = path.join(__dirname, '/src/public/img/headers');
-app.get('/api/headingImg', (req, res) => {
-    fs.readdir(headingImgDir, (err, files) => {
-        if(err){
-            console.log(`Error retrieving header images folder: ${err}`);
-            return;
-        }
-
-        // remove README.txt if present
-        let index = files.indexOf("README.txt");
-        if(index >= 0) files.splice(index, 1);
-
-        res.send(JSON.stringify(files));
-    })
-})
-
-
-/** GET: returns an array of the team image names */
-const teamImgDir = path.join(__dirname, '/src/public/img/team');
-app.get('/api/team', (req, res) => {
-    fs.readdir(teamImgDir, (err, files) => {
-        if(err){
-            console.log(`Error retrieving team images folder: ${err}`);
-            return;
-        }
-
-        // remove README.txt if present
-        let index = files.indexOf("README.txt");
-        if(index >= 0) files.splice(index, 1);
-
-        res.send(JSON.stringify(files));
-    })
-})
-
-
-/** GET: returns latest Facebook posts? */
-app.get('/api/facebook', (req, res) => {
-
-    res.send("Hello, World!");
-})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/public/index.html'));
@@ -91,14 +31,31 @@ app.get('/enquire', (req, res) => {
     const email = query.emailField;
     const message = query.messageField;
 
-    if((name != undefined) && 
-       (email != undefined) && 
-       (message != undefined)){
+    if((name != undefined) && (email != undefined) && (message != undefined)){ // if all of the field were valid
 
-        // Process the data
-        console.log('Name:', name);
-        console.log('Email:', email);
-        console.log('Message:', message);
+        const enquirySender = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              user: 'bellsbestcutsenquiry@gmail.com',
+              pass: '72ALvq%RLG'
+            }
+        });
+
+        const mailOptions = {
+            from: 'bellsbestcutsenquiry@gmail.com',
+            to: 'callumsharman64@gmail.com',
+            subject: 'Enquiry from BellsBestCuts.com',
+            text: `name: ${name}, email: ${email}, message "${message}"`
+        };
+
+
+        enquirySender.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+        });
 
         // Redirect home
         res.redirect('/');
@@ -112,3 +69,64 @@ app.get('/enquire', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
+
+
+
+/*=================================== API ===================================*/
+
+/** GET: returns an array of heading image names */
+const headingImgDir = path.join(__dirname, '/src/public/img/headers');
+app.get('/api/headingImg', (req, res) => {
+    fs.readdir(headingImgDir, (err, files) => {
+        if(err){
+            console.log(`Error retrieving header images folder: ${err}`);
+            return;
+        }
+
+        // remove README.txt if present
+        let index = files.indexOf("README.txt");
+        if(index >= 0) files.splice(index, 1);
+
+        res.send(JSON.stringify(files));
+    })
+})
+
+/** GET: returns an array of the team image names */
+const teamImgDir = path.join(__dirname, '/src/public/img/team');
+app.get('/api/team', (req, res) => {
+    fs.readdir(teamImgDir, (err, files) => {
+        if(err){
+            console.log(`Error retrieving team images folder: ${err}`);
+            return;
+        }
+
+        // remove README.txt if present
+        let index = files.indexOf("README.txt");
+        if(index >= 0) files.splice(index, 1);
+
+        res.send(JSON.stringify(files));
+    })
+})
+
+/** GET: returns an array of gallery image names */
+const galleryDir = path.join(__dirname, '/src/public/img/gallery');
+app.get('/api/gallery', (req, res) => {
+    fs.readdir(galleryDir, (err, files) => {
+        if(err){
+            console.log(`Error retrieving gallery folder: ${err}`);
+            return;
+        }
+
+        // remove README.txt if present
+        let index = files.indexOf("README.txt");
+        if(index >= 0) files.splice(index, 1);
+        
+        res.send(JSON.stringify(files));
+    })
+})
+
+/** GET: returns latest Facebook posts? */
+app.get('/api/facebook', (req, res) => {
+
+    res.send("Hello, World!");
+})
